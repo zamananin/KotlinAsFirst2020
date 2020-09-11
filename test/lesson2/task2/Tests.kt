b616d3f5f1b2dade88f3a71973dd0587e4ae2176
Александр <zamyatin.av@edu.spbstu.ru>
2020-09-11 17:29:49 +0300

    Исправлен Lesson 2 task 2

--- test/lesson2/task2/Tests.kt
+++ test/lesson2/task2/Tests.kt
@@ -28,6 +28,7 @@ class Tests {
         assertTrue(queenThreatens(3, 6, 7, 6))
         assertTrue(queenThreatens(8, 1, 1, 8))
         assertFalse(queenThreatens(7, 6, 5, 7))
+        assertTrue(queenThreatens(4, 2, 3, 1))
     }
 
     @Test
